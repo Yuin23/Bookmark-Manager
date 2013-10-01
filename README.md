a54de4524c50800f20665395aa194e87da1d2af4
Yuin <yhuang1223@gmail.com>
2013-10-01 03:46:42 +0100

    edited README.md

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-Bookmark Manager
+##	Bookmark manager	##
 
-Task:
+
+##### Task:	#####
 This week's project is a bookmark manager. The goal is to expose you to the following aspects of web development, in addition to what we've studied before:
 
 	Integration testing: Capybara and maybe Cucumber 
@@ -8,11 +9,11 @@ This week's project is a bookmark manager. The goal is to expose you to the foll
 	Security considerations 
 
 
-Description:
+##### Description:	#####
 A bookmark manager is a website to maintain a collection of links, organised by tags. You can use it to save a webpage you found useful. You can add tags to the webpages you saved to find them later. You can browse links other users have added.
 
 
-The website will have the following options:
+The website will have the following features:
 
 	Show a list of links from the database
 	Add new links
--- app/server.rb
+++ app/server.rb
@@ -10,6 +10,8 @@ require_relative 'controllers/links'
 require_relative 'controllers/tags'
 require_relative 'controllers/application'
 
+require_relative 'data_mapper_setup'
+
 
 enable :sessions
 set :session_secret, 'super super secret'
@@ -18,28 +20,28 @@ use Rack::Flash
 set :partial_template_engine, :erb
 
 
-env = ENV["RACK_ENV"] || "development"
-# we're telling datamapper to use a postgres database on localhost. 
-# The name will be "bookmark_manager_test" or "bookmark_manager_development"
-# depending on the environment 
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
+# env = ENV["RACK_ENV"] || "development"
+# # we're telling datamapper to use a postgres database on localhost. 
+# # The name will be "bookmark_manager_test" or "bookmark_manager_development"
+# # depending on the environment 
+# DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 
-require_relative 'models/link'	# this needs to be done after datamapper is intialised 
-require_relative 'models/tag'
-require_relative 'models/user'	# DO NOT FORGET !!  #
+# require_relative 'models/link'	# this needs to be done after datamapper is intialised 
+# require_relative 'models/tag'
+# require_relative 'models/user'	# DO NOT FORGET !!  #
 
-DataMapper.finalize 	# after declaring models, finalise them
+# DataMapper.finalize 	# after declaring models, finalise them
 
-DataMapper.auto_upgrade! # tell datamapper to create database tables. 
-
-
+# DataMapper.auto_upgrade! # tell datamapper to create database tables. 
 
 
 
-helpers do 
-	def current_user
-		@current_user ||=User.get(session[:user_id]) if session[:user_id]
-	end 
-end 
+
+
+# helpers do 
+# 	def current_user
+# 		@current_user ||=User.get(session[:user_id]) if session[:user_id]
+# 	end 
+# end 
 
 
